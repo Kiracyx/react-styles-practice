225661e118235a0b6dccf809c5935f6f820eedff
Mykola Khomiakov <vkontemail4@gmail.com>
2024-09-11 15:37:56 +0300

    Import sass files

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,55 @@
 import React, { Component } from "react";
-import "./App.css";
+import ThemeButton from "./components/ThemeButton";
+import "./App.sass";
 
 class App extends Component {
-  render() {
-    const test = "test text";
+  constructor(props) {
+    super(props);
 
-    return <div>{test}</div>;
+    this.state = {
+      theme: true,
+    };
+  }
+
+  toggleTheme = () => {
+    this.setState((prevState) => {
+      // let result;
+      if (prevState.theme === true) {
+        return {
+          theme: false,
+        };
+      } else {
+        return {
+          theme: true,
+        };
+      }
+      /* prevState.theme === true ? (result = false) : (result = true);
+
+      return {
+        theme: { result },
+      }; */
+    });
+  };
+
+  render() {
+    const test = "Change theme";
+    const main = "main";
+
+    let themeClass = "lightTheme";
+    if (this.state.theme === false) {
+      themeClass = "darkTheme";
+    }
+
+    return (
+      <>
+        <div className={main}>
+          <div className={themeClass}>
+            <p>{test}</p>
+          </div>
+        </div>
+        <ThemeButton toggle={this.toggleTheme} theme={this.state.theme} />
+      </>
+    );
   }
 }
 
--- src/components/ThemeButton/index.jsx
+++ src/components/ThemeButton/index.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import styles from "./ThemeButton.module.sass";
 
 const ThemeButton = (props) => {
-  const { toggle } = props;
+  const { toggle, theme } = props;
+
+  let themeClass = styles.btn + " lightTheme";
+  if (theme !== false) {
+    themeClass = styles.btn + " darkTheme";
+  }
 
   return (
-    <button className="btn" onClick={toggle}>
+    <button className={themeClass} onClick={toggle}>
       Click to change
     </button>
   );
